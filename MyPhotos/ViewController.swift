3aba07ad9627c22c7de6a156e09c8b376f119c2e
Artem Solovenko <a.solovenko@gmail.com>
2016-11-01 23:01:10 +0700

    Initial commit

---
+++ MyPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPhotos
+//
+//  Created by Артем Соловьенко on 01.11.16.
+//  Copyright © 2016 Artem Solovenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
